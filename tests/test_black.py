4b2ca73315f0805270db0874e125bea00553f655
Grigorios Giannakopoulos <ggiannakopo3@bloomberg.net>
2019-07-29 09:15:35 +0100

    Add tests for `cdef` blocks
    Add tests to ensure that `cdef` blocks parse.

--- tests/test_black.py
+++ tests/test_black.py
@@ -1662,6 +1662,10 @@ class BlackTestCase(unittest.TestCase):
         src_text, _ = read_data("cython_parse_tests/cdef_class.pyx")
         black.lib2to3_parse(src_text, {TargetVersion.CYTHON})
 
+    def test_cython_parse_cdef_block(self) -> None:
+        src_text, _ = read_data("cython_parse_tests/cdef_block.pyx")
+        black.lib2to3_parse(src_text, {TargetVersion.CYTHON})
+
 
 if __name__ == "__main__":
     unittest.main(module="test_black")
