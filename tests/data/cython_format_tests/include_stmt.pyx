521573ab1dfcd79d921a57ea3c6cf5c3e7d98337
Grigorios Giannakopoulos <ggiannakopo3@bloomberg.net>
2019-07-31 10:47:40 +0100

    Add testing for formatting `include_stmt`s
    Add testing for formatting `include_stmt`s, with comments. No extra work
    is required since proper formatting is covered by the default rules
    (just like `raise` statements)

---
+++ tests/data/cython_format_tests/include_stmt.pyx
@@ -0,0 +1,11 @@
+include             "amazing.pxi"
+include    "fantastic.pxi"  # aaa
+
+include  "wonderful.pxi"        # aaa
+
+# output
+
+include "amazing.pxi"
+include "fantastic.pxi"  # aaa
+
+include "wonderful.pxi"  # aaa
