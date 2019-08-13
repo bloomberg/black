673b083a74a372250db69e57600021853529ac19
Grigorios Giannakopoulos <ggiannakopo3@bloomberg.net>
2019-08-13 16:49:41 +0100

    Add tests for fused type definitions
    Add tests to ensure that fused type definitions parse.

--- tests/test_black.py
+++ tests/test_black.py
@@ -1700,6 +1700,10 @@ class BlackTestCase(unittest.TestCase):
         src_text, _ = read_data("cython_parse_tests/struct_union.pyx")
         black.lib2to3_parse(src_text, {TargetVersion.CYTHON})
 
+    def test_cython_parse_fuseddef(self) -> None:
+        src_text, _ = read_data("cython_parse_tests/fuseddef.pyx")
+        black.lib2to3_parse(src_text, {TargetVersion.CYTHON})
+
     def test_cython_format_cimport(self) -> None:
         black.monkey_patch_cython_symbols()
         source, expected = read_data("cython_format_tests/cimport.pyx")
